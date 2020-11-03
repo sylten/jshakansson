272ad7d61205fd985e7035b3eca5243a44379e7e
Jonas Siltamäki Håkansson <jonas@Jonass-MacBook-Pro.local>
2020-11-03 18:20:38 +0100

    Meta tags

--- common/layout/Layout.js
+++ common/layout/Layout.js
@@ -7,7 +7,21 @@ export default function Layout({children}) {
     <div className={styles.Layout}>
       <Head>
         <title>Jonas Siltamäki Håkansson</title>
-        <link rel="icon" href="/favicon.ico" />
+        <meta name="title" property="title" content="Jonas Siltamäki Håkansson" />
+        <meta property="og:title" content="Jonas Siltamäki Håkansson" />
+        <meta name="description" property="description" content="My portfolio of personal and professional projects that I've worked on." />
+        <meta property="og:description" content="My portfolio of personal and professional projects that I've worked on." />
+        <meta property="og:image" content="https://jshakansson.se/assets/jshakansson-150.png" />
+        <meta property="og:image:url" content="https://jshakansson.se/assets/jshakansson-150.png" />
+        <meta property="og:image:secure_url" content="https://jshakansson.se/assets/jshakansson-150.png" />
+        <meta property="og:image:type" content="image/png" />
+        <meta property="og:image:alt" content="Jonas Portfolio logo" />
+        <meta property="og:type" content="website" />
+        <meta property="og:locale" content="sv_SE" />
+        <meta property="og:site_name" content="Jonas Siltamäki Håkansson" />
+        <meta property="og:url" content="https://jshakansson.se" />
+        <meta name="google-site-verification" content="RZ_40JsTzlDejUohOlhrK4KzHir-YRa3kAoSdDAyrrI" />
+        <link rel="icon" type="image/x-icon" href="favicon.ico?v=7" />
       </Head>
       <div className={styles.Content}>
         {children}
